77e3b54fb4342442a83c772391f533bca81624d7
olashina201 <olashina201@gmail.com>
2023-07-01 16:34:20 +0100

    update

--- predict.py
+++ predict.py
@@ -5,43 +5,45 @@ import cv2
 import sys
 import config
 
-# Load model da train
-my_model = load_model(os.path.join("models","ICT-w-max.hdf5"))
-# my_model = load_model(os.path.join("models","VGG16-w-max.hdf5"))
-# my_model = load_model(os.path.join("models","ResNet50-w-max.hdf5"))
+# Load the trained model
+my_model = load_model(os.path.join("models", "ICT-w-max.hdf5"))
 
 import pickle
-file = open('le.pkl', 'rb')
-le = pickle.load(file)
-file.close()
+with open('le.pkl', 'rb') as file:
+    le = pickle.load(file)
 
-# Doc anh tu dong lenh
+# Read image from command line argument
 image_org = cv2.imread(sys.argv[1])
 
-# Resize
-image = image_org.copy()
-image = cv2.resize(image, dsize=(config.image_size, config.image_size))
+# Resize image
+image = cv2.resize(image_org, dsize=(config.image_size, config.image_size))
 
-# chuyen sang dinh dang tensor
+# Convert image to tensor format
 image = np.expand_dims(image, axis=0)
 
-# du doan
+# Perform prediction
 predict = my_model.predict(image)
+
+# Get the predicted class index
+predicted_class_index = np.argsort(predict)[0][-2]
+
+# Get the predicted class label
+predicted_class_label = le.inverse_transform([predicted_class_index])[0]
+
 print(predict)
 print(np.argsort(predict))
-print(np.argsort(predict)[0][-2])
-print(le.inverse_transform([np.argsort(predict)[0][-2]]))
-print("This picture is: ", le.inverse_transform([np.argmax(predict)]))
+print(predicted_class_index)
+print(predicted_class_label)
+print("This picture is:", predicted_class_label)
 
-# hien thi anh
+# Display the image with the predicted label
 font = cv2.FONT_HERSHEY_SIMPLEX
 org = (50, 50)
 fontScale = 1
 color = (255, 0, 0)
 thickness = 2
-# viet ket qua
-cv2.putText(image_org, le.inverse_transform([np.argmax(predict)])[0], org, font,
-                    fontScale, color, thickness, cv2.LINE_AA)
 
-cv2.imshow("Result",image_org)
+cv2.putText(image_org, predicted_class_label, org, font, fontScale, color, thickness, cv2.LINE_AA)
+
+cv2.imshow("Result", image_org)
 cv2.waitKey()
